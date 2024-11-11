8aac65938e0d24825ed1199f5c632b042ce461ed
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-11-11 21:09:44 +0000

    Добавлен тред для запуска GC.

--- src/server.lisp
+++ src/server.lisp
@@ -2,15 +2,33 @@
   (:use #:cl)
   (:import-from #:log4cl)
   (:import-from #:woo)
+  (:import-from #:bordeaux-threads)
+  (:import-from #:lparallel)
   (:import-from #:app/logging)
   (:import-from #:app/app
                 #:disk-analyzer)
+  (:import-from #:reblocks/server)
   (:import-from #:app/slynk
                 #:start-slynk-if-needed)
   (:export
    #:start))
 (in-package #:app/server)
 
+
+(defvar *gc-thread* nil)
+
+
+(defun call-gc ()
+  (loop do (sb-ext:gc :full t)
+           (sleep 60)))
+
+
+(defun start-gc-thread ()
+  (when (or (null *gc-thread*)
+            (not (bordeaux-threads:thread-alive-p *gc-thread*)))
+    (setf *gc-thread*
+          (bordeaux-threads:make-thread #'call-gc
+                                        :name "Periodical GC"))))
 
 (defun make-lparallel-kernel-if-needed ()
   (let ((num-workers (parse-integer
@@ -22,13 +40,12 @@
 
 
 (defun load-local-config ()
-  (let ((filename (asdf/system:system-relative-pathname :app ".local.lisp")))
+  (let ((filename (asdf/system:system-relative-pathname :app ".local-config.lisp")))
     (when (probe-file filename)
       (load filename))))
 
 
-(defun start (&key (port 8080)
-		   (interface "localhost"))
+(defun start (&key (port 8080) (interface "localhost"))
   ;; Just to suppres debug logs to TTY from Reblocks.
   ;; I'll need to fix Reblocks to prohibit it from
   ;; configure logging if they are already configured.
@@ -39,7 +56,12 @@
   (reblocks/server:start :port port
 			 :interface interface
                          :apps 'disk-analyzer
-			 :server-type :woo)
+                         ;; Woo fails for version
+                         ;; woo-20240910140304 and SBCL "2.4.5"
+                         ;; Ultralisp version 20240911125500
+			 ;; :server-type :woo
+                         )
+  (start-gc-thread)
   (app/logging::setup)
   (log:info "Server started"))
 
